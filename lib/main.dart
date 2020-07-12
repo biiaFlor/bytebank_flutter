823ba27004478e6f6a21ece6a51d958d710e3ed5
unknown <biiaflor_ifsp@hotmail.com>
2020-07-12 12:44:49 -0300

    Adição da barra superior e do botão de adição de transferencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Campo text 1 - column 1',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Campo text 2 - column 1',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Campo text 3 - column 1',
-            textDirection: TextDirection.ltr,
-          ),
-          Column(children: <Widget>[
-            Text(
-              'Campo text 1 - column 2',
-              textDirection: TextDirection.ltr,
-            ),
-          ])
-        ],
-      ), //Column
-    );
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferências'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
