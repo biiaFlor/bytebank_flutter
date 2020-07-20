b2212c9684654ec5bc7777fc3a5a74d6278d9642
unknown <biiaflor_ifsp@hotmail.com>
2020-07-20 19:00:18 -0300

    Adição de variaveis para armazenamento dos valores informados no formulario, validação para salvar dados e alerta em caso de erro

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,10 @@ class Transferencia {
 }
 
 class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,6 +72,19 @@ class FormularioTransferencia extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
+              controller: _controladorCampoNumeroConta,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  labelText: 'Número da Conta', hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoValor,
               style: TextStyle(
                 fontSize: 24.0,
               ),
@@ -78,12 +95,46 @@ class FormularioTransferencia extends StatelessWidget {
               keyboardType: TextInputType.number,
             ),
           ),
-          TextField(),
           RaisedButton(
             child: Text('Confirmar'),
-          ),
+            onPressed: () {
+              final int numeroConta =
+                  int.tryParse(_controladorCampoNumeroConta.text);
+              final double valor = double.tryParse(_controladorCampoValor.text);
+
+              if (numeroConta != null && valor != null) {
+                final transferenciaCriada = Transferencia(valor, numeroConta);
+              } else {
+                showAlertDialog(context);
+              }
+            },
+          )
         ],
       ),
     );
   }
+}
+
+showAlertDialog(BuildContext context) {
+  // configura o button
+  Widget botao = FlatButton(
+      child: Text("Ok"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      });
+  // Caixa de Alerta
+  AlertDialog alerta = AlertDialog(
+    title: Text("Ops, Parece que temos problemas!"),
+    content: Text("Todos os campos devem ser preenchidos."),
+    actions: [
+      botao,
+    ],
+  );
+  // exibe a caixa de alerta
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alerta;
+    },
+  );
 }
