8ece9fa64ff40e2b0a372a814b11e4878f227dfa
unknown <biiaflor_ifsp@hotmail.com>
2020-07-25 18:01:48 -0300

    Altração nas nomenclaturas dos arquivos, funções e variaveis para seguir o  metodo explicado nas video aulas

--- lib/Models/Contato.dart
+++
@@ -1,7 +0,0 @@
-class Contato {
-  final int IdContato;
-  final String nome;
-  final int numeroConta;
-
-  Contato(this.IdContato, this.nome, this.numeroConta);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
-import 'package:bytebank/DataBase/CriaBanco.dart';
-import 'package:bytebank/Models/Contato.dart';
-import 'package:bytebank/Screens/Dashboard.dart';
+import 'package:bytebank/database/app_database.dart';
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/contacts_list.dart';
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
+
+import 'models/contact.dart';
 
 void main() {
   runApp(BytebankApp());
-  insertContato(Contato(1, 'Beatriz', 1000)).then((IdContato) {
-    selectContatos().then((contatos) {});
-  });
+  findAll().then((contacts) => debugPrint(contacts.toString()));
 }
 
-//Tela Inicial
 class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
