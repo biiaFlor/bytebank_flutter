04fac85905dda1678fbd4da9bd81d9725b197a95
unknown <biiaflor_ifsp@hotmail.com>
2020-08-03 14:42:16 -0300

    Refatoração das funções de manipulação dos dados do banco

--- lib/DataBase/app_database.dart
+++ lib/DataBase/app_database.dart
@@ -19,28 +19,64 @@ Future<Database> createDatabase() async {
   );
 }
 
-Future<int> save(Contact contact) {
-  return createDatabase().then((db) {
-    final Map<String, dynamic> contactMap = Map();
-    contactMap['name'] = contact.name;
-    contactMap['account_number'] = contact.accountNumber;
-    return db.insert('contacts', contactMap);
-  });
+//return getDatabasesPath().then((dbPath) {
+//   final String path = join(dbPath, 'bytebank.db');
+//   return openDatabase(
+//     path,
+//     onCreate: (db, version) {
+//       db.execute('CREATE TABLE contacts('
+//           'id INTEGER PRIMARY KEY, '
+//           'name TEXT, '
+//           'account_number INTEGER)');
+//     },
+//     version: 1,
+//     // onDowngrade: onDatabaseDowngradeDelete,
+//   );
+// });
+
+Future<int> save(Contact contact) async {
+  final Database db = await createDatabase();
+
+  final Map<String, dynamic> contactMap = Map();
+  contactMap['name'] = contact.name;
+  contactMap['account_number'] = contact.accountNumber;
+  return db.insert('contacts', contactMap);
+
+  // return createDatabase().then((db) {
+  //   final Map<String, dynamic> contactMap = Map();
+  //   contactMap['name'] = contact.name;
+  //   contactMap['account_number'] = contact.accountNumber;
+  //   return db.insert('contacts', contactMap);
+  // });
 }
 
 Future<List<Contact>> findAll() async {
-  return createDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = List();
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact = Contact(
-          map['id'],
-          map['name'],
-          map['account_number'],
-        );
-        contacts.add(contact);
-      }
-      return contacts;
-    });
-  });
+  final Database db = await createDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+
+  final List<Contact> contacts = List();
+  for (Map<String, dynamic> row in result) {
+    final Contact contact = Contact(
+      row['id'],
+      row['name'],
+      row['account_number'],
+    );
+    contacts.add(contact);
+  }
+  return contacts;
+
+  // return createDatabase().then((db) {
+  //   return db.query('contacts').then((maps) {
+  //     final List<Contact> contacts = List();
+  //     for (Map<String, dynamic> map in maps) {
+  //       final Contact contact = Contact(
+  //         map['id'],
+  //         map['name'],
+  //         map['account_number'],
+  //       );
+  //       contacts.add(contact);
+  //     }
+  //     return contacts;
+  //   });
+  // });
 }
